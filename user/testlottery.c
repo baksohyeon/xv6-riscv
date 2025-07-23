785c728af65bc654bc12b51892836a3599e151f7
Dorito <bori12370@gmail.com>
2025-07-23 19:52:55 +0900

    fix: enhencement of console

--- user/testlottery.c
+++ user/testlottery.c
@@ -6,7 +6,7 @@
 // Test the lottery scheduler with 3:2:1 ticket ratio
 // Process A: 30 tickets, Process B: 20 tickets, Process C: 10 tickets
 
-#define TEST_DURATION 1000  // Number of iterations to run
+#define TEST_DURATION 5000  // Number of iterations to run
 #define PROCESS_A_TICKETS 30
 #define PROCESS_B_TICKETS 20  
 #define PROCESS_C_TICKETS 10
@@ -18,27 +18,23 @@ worker_process(char process_name, int tickets)
   
   // Set the number of tickets for this process
   if(settickets(tickets) < 0) {
-    fprintf(2, "Failed to set tickets for process %c\n", process_name);
     exit(1);
   }
   
-  fprintf(2, "Process %c: Set tickets to %d, PID=%d\n", process_name, tickets, getpid());
-  
-  // Do some work to consume CPU time
+  // Do some work to consume CPU time - more intensive work
   for(i = 0; i < TEST_DURATION; i++) {
     // Busy work - just consume CPU cycles
     volatile int dummy = 0;
-    for(int j = 0; j < 10000; j++) {
-      dummy += j;
+    for(int j = 0; j < 50000; j++) {  // Increased work per iteration
+      dummy += j * j;  // More computational work
     }
     
     // Yield occasionally to allow scheduler to make decisions
-    if(i % 100 == 0) {
-      sleep(1);  // Brief sleep to allow other processes to run
+    if(i % 50 == 0) {  // Yield more frequently
+      sleep(1);
     }
   }
   
-  fprintf(1, "Process %c: Finished work\n", process_name);
   exit(0);
 }
 
@@ -53,24 +49,35 @@ print_process_stats()
     return;
   }
   
-  fprintf(1, "\nProcess Statistics:\n");
+  fprintf(1, "\nProcess Statistics (All active processes):\n");
   fprintf(1, "PID\tTickets\tTicks\tRatio\n");
   fprintf(1, "---\t-------\t-----\t-----\n");
   
   int total_ticks = 0;
+  int test_process_ticks = 0;
+  
+  // Calculate total ticks from all processes
   for(i = 0; i < NPROC; i++) {
     if(ps.inuse[i] && ps.ticks[i] > 0) {
       total_ticks += ps.ticks[i];
     }
   }
   
+  // Show all processes with ticks
   for(i = 0; i < NPROC; i++) {
     if(ps.inuse[i] && ps.ticks[i] > 0) {
       int ratio = total_ticks > 0 ? (ps.ticks[i] * 100) / total_ticks : 0;
       fprintf(1, "%d\t%d\t%d\t%d%%\n", ps.pid[i], ps.tickets[i], ps.ticks[i], ratio);
+      
+      // Count ticks from test processes (those with non-default ticket counts)
+      if(ps.tickets[i] == PROCESS_A_TICKETS || ps.tickets[i] == PROCESS_B_TICKETS || ps.tickets[i] == PROCESS_C_TICKETS) {
+        test_process_ticks += ps.ticks[i];
+      }
     }
   }
+  
   fprintf(1, "\nTotal ticks: %d\n", total_ticks);
+  fprintf(1, "Test process ticks: %d\n", test_process_ticks);
 }
 
 int
@@ -112,13 +119,17 @@ main(int argc, char *argv[])
   }
   
   // Parent process waits for all children to complete
-  fprintf(1, "Waiting for processes to complete...\n");
+  fprintf(1, "Starting processes...\n");
+  fprintf(1, "Process A (PID %d): 30 tickets\n", pid_a);
+  fprintf(1, "Process B (PID %d): 20 tickets\n", pid_b);  
+  fprintf(1, "Process C (PID %d): 10 tickets\n", pid_c);
+  fprintf(1, "Waiting for processes to complete...\n\n");
   
   wait(0);  // Wait for process A
   wait(0);  // Wait for process B  
   wait(0);  // Wait for process C
   
-  fprintf(1, "\nAll processes completed. Final statistics:\n");
+  fprintf(1, "All processes completed. Final statistics:\n");
   print_process_stats();
   
   fprintf(1, "\nLottery scheduler test completed.\n");
