9325b0a81bb20225163df6615c233e46161742c1
Dorito <bori12370@gmail.com>
2025-07-25 10:38:39 +0900

    fix: change the data type of the process ticks counter to a double to allow finer-grained tracking of CPU usage
    Makes the process tickets counter not atomic since they are only read/written with the process lock held.

--- kernel/proc.h
+++ kernel/proc.h
@@ -94,8 +94,8 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-  _Atomic uint tickets;         // Number of tickets for lottery scheduling
-  _Atomic uint ticks;          // Number of times this process has been chosen
+  uint tickets;         // Number of tickets for lottery scheduling
+  double ticks;          // Number of times this process has been chosen
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
