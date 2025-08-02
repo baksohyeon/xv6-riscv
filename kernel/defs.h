0821e4d4ea6689027884f0ecfa63423e078cb7fd
Dorito <bori12370@gmail.com>
2025-08-02 20:31:53 +0900

    feat: add the function prototypes to defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -181,6 +181,8 @@ pte_t *         walk(pagetable_t, uint64, int);
 uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
+int             mprotect(uint64, int);
+int             munprotect(uint64, int);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 
 // plic.c
